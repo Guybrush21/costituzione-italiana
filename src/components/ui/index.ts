64eb7d25d6f9061469fc15d5e5ade475248efdf5
Nicholas Pellizer <nicholasp21@gmail.com>
2026-03-18 15:38:05 +0100

    refactor: icons are now isolated

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -8,4 +8,7 @@ export { default as Badge } from './Badge.astro';
 export { default as Breadcrumbs } from './Breadcrumbs.astro';
 export { default as Card } from './Card.astro';
 export { default as Container } from './Container.astro';
+export { default as Icon } from './Icon.astro';
 export { default as LinkButton } from './LinkButton.astro';
+
+export type { IconName } from './Icon.astro';
